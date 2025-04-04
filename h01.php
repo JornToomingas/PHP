d503db06f80f7b12b71df93b8eb1517791a1ab11
JornToomingas <jmarkus@hkhk.edu.ee>
2025-04-04 13:00:40 +0300

    Create h01.php

---
+++ h01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<body>
+<?php
+//Jörn Markus Toomingas, ül 1, 04.04.25
+
+$enimi = 'Toomas'
+$synnia = '2000'
+$tahtkuju = 'Vähk'
+
+echo $enimi, $synnia, $tahtkuju
+
+
+
+
+?>
+</body>
+</html>
