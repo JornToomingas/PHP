01b7d345a2da03c96924aad39e53161ea899f1f5
JornToomingas <jmarkus@hkhk.edu.ee>
2025-09-26 18:09:14 +0300

    Update yl6.php

--- yl6.php
+++ yl6.php
@@ -3,28 +3,51 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap</title>
+    <title>Harjutus6</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
     <h1>Harjutus 06</h1>
     <?php
-    //tekita tüdrukute ja poiste massiivid (võrdsed)
-    //väljasta poiste ja tüdrukute paarid erinevatel ridadel
-      $t = array("juuli","maali","kati");
-      $p = array("ago","marko","mati");
+     //Massiivid ja tsüklid
 
-      for ($i=0; $i < count($t); $i++) {
-        echo $t[$i]." - ".$p[$i]."<br>";
-      } 
+    $tudrukud = ['mari', 'kati', 'marion', 'kerli', 'maria'];
+    $poisid   = ['gabriel','mario','üllar','joonas','henrik']; 
 
+    for ($i = 0; $i < count($tudrukud); $i++) {
+        echo "<div style='margin-bottom:4px;'>";
+        echo "<span style='display:inline-block; width:100px;'>{$tudrukud[$i]}</span>";
+        echo "<span style='display:inline-block; width:100px;'>{$poisid[$i]}</span>";
+        echo "</div>";
+    }
+
+echo "<br>";
+
+    //Massiivid ja tsüklid (Bonus)
+    shuffle($tudrukud);
+    shuffle($poisid);
+
+    for ($i = 0; $i < count($tudrukud); $i++) {
+        echo "<div style='margin-bottom:4px;'>";
+        echo "<span style='display:inline-block; width:100px;'>{$tudrukud[$i]}</span>";
+        echo "<span style='display:inline-block; width:100px;'>{$poisid[$i]}</span>";
+        echo "</div>";
+    }
+echo "<br>";
     //Kolmega jaguvad
       for ($i=1; $i <= 100 ; $i++) {
         if ($i%3==0) {
           echo $i. "<br>";
         }
       }
-
+echo "<br>";
+    //Kahanev
+      for ($i = 10; $i >= 1; $i--) {
+    if ($i % 2 == 0) {
+        echo $i . "<br>";
+    }
+    }
+echo "<br>";
     //10x10 tärnid (ruut)
       for ($i=1; $i <= 100; $i++) {
           echo"*";
@@ -32,19 +55,19 @@
               echo "<br>";
           }
       }
-
+echo "<br>";
     //koosta tärnidest horisontaalne rida
       $tarnideArv = 10;
 
       for ($i = 0; $i < $tarnideArv; $i++) {
           echo "*";
       }
-
+echo "<br>";
     //koosta tärnidest vertikaalne rida
       for($rida=1; $rida<=10; $rida++){ 
         echo '*<br>';
       }
-
+echo "<br>";
     //loo arvud 1-100
     //loo pärast iga 10 arvu reavahetus
     //lisa iga arvu järele punkt (N: 1. 2. 3. jne…)
